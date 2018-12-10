7f2bee955433c81677a081d04009a8ecedf190a4
Kyntal <clandres1985@gmail.com>
2018-12-10 01:36:57 -0300

    funcionando conexion entre paginas

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,20 @@
 from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views
+from django.contrib.auth.views import logout
 from . import views
+from .views import nuevoproducto
+from .views import registroventa
+from .views import vendedor
 
 urlpatterns = [
-	url(r'^$', views.administrador, name='home'),
 	url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
+	url(r'social-auth/', include('social_django.urls', namespace='social')),
+	url(r'^$', views.administrador, name='inicio'),
+	url(r'^nuevatienda/$', views.nuevatienda, name = 'nuevatienda'),
+	url(r'^nuevo-producto/$', nuevoproducto, name = 'nuevoproducto'),
+	url(r'^registro-venta/$', registroventa, name = 'registroventa'),
+	url(r'^vendedor/$', vendedor, name = 'vendedor'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def administrador(request):
     return render(request, 'blog/administrador.html', {})
 
+
+def login(request):
+
+    return render(request, 'blog/registration/login.html')
+
+def logout(request):
+    return redirect('/blog')
+
+def nuevatienda(request):
+    return render(request, 'blog/nuevatienda.html')
+
+def nuevoproducto(request):
+    return render(request, 'blog/nuevo_producto.html')
+
+def registroventa(request):
+    return render(request, 'blog/registro_venta.html')
+
+def vendedor(request):
+    return render(request, 'blog/vendedor.html')
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
